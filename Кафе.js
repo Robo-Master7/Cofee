6b1f1536da223baa8b6bba776195bcb219f53631
Robo-Master7 <136693201+Robo-Master7@users.noreply.github.com>
2023-07-15 16:51:05 +0300

    1.0.1

---
+++ Кафе.js
@@ -0,0 +1,9 @@
+let form = document.querySelector('.form');
+let logi = document.querySelector('.log');
+let passw = document.querySelector('.pass');
+let voi = document.querySelector('.zac');
+let but = document.querySelector('.bu');
+
+voi.addEventListener('click',()=>{
+    form.style.visibility='visible'
+})
