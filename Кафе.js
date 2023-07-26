8efc21e33b547734c53e674d42c00fa792f2c163
Robo-Master7 <136693201+Robo-Master7@users.noreply.github.com>
2023-07-26 12:46:53 +0300

    1.0.2

--- Кафе.js
+++ Кафе.js
@@ -1,9 +1,36 @@
-let form = document.querySelector('.form');
+let form1 = document.querySelector('.form1');
+let form2 = document.querySelector('.form2');
 let logi = document.querySelector('.log');
 let passw = document.querySelector('.pass');
 let voi = document.querySelector('.zac');
-let but = document.querySelector('.bu');
+let but1 = document.querySelector('.bu1');
+let but2 = document.querySelector('.bu2');
 
 voi.addEventListener('click',()=>{
-    form.style.visibility='visible'
+    form1.style.visibility='visible'
 })
+
+but1.addEventListener('click',()=>{
+    if(logi.value==''){
+        alert('Заполните поля')
+    }else{
+        form1.style.visibility='hidden'
+        form2.style.visibility='visible'
+    }
+})
+
+but2.addEventListener('click',()=>{
+    if(passw.value==''){
+        alert('Заполните поля')
+    }else{
+        form2.style.visibility='hidden'
+        alert('Скоро мы вам позвоним чтобы подтведить заказ')
+    }
+})
+
+
+
+
+
+//form.style.visibility='visible'
+       // alert('Скоро мы вам позвоним чтобы подтведить заказ')
